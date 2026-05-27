7bf52de53ea2dfd110914dfdd35aff53effe80ef
thdtprl52-hash <thdtprl52@gmail.com>
2026-05-28 00:06:42 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "work-confirmation-app-v1";
+const CACHE_NAME = "work-confirmation-app-blank-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -23,6 +23,12 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
